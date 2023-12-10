ad19d09a29684806ef513512115e5ce5921e675c
arasaix <xavi@DESKTOP-PMTH6V6>
2023-12-10 22:24:35 +0100

    day 2 part one

--- day 2/Part One/main.py
+++ day 2/Part One/main.py
@@ -2,25 +2,21 @@ import re
 
 file = open('../input.txt', 'r')
 game_dict = {}
-pattern1 = r'(\d+)(?=:)'
-result = []
-i = 0
-aux_list = []
-for j in range(100):
-    aux_list.append(j)
+pattern_game = r'(\d+)(?=:)'
+result = list(range(1, 101))
+
 for line in file:
-    matches = re.findall(pattern1, line)
+    matches = re.findall(pattern_game, line)
     if matches:
         game = matches[0]
-        print(game)
         pattern = r'(\d+)\s(\w+)'
         matches = re.findall(pattern, line)
-        print(matches)
         for match in matches:
             digit = int(match[0])
             color = match[1]
-            if (color == 'red' and digit > 13) or (color == 'blue' and digit > 14) or (color == 'green' and digit > 15):
-                aux_list.remove(int(game))
+            if (color == 'red' and digit > 12) or (color == 'green' and digit > 13) or (color == 'blue' and digit > 14):
+                result.remove(int(game))
                 break
-print("Result:", result)
-print(sum(result))
+print("Games:", result)
+print("Sum od IDs:", sum(result))
+
